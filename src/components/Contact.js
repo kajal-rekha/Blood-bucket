902511e2f4c3ccfce71386e37217bcc224e4f373
kajal-2011 <kajalrekha888@gmail.com>
2023-02-19 08:25:16 +0600

    fotter done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,4 @@
-const Form = () => {
+const Contact = () => {
   return (
     <form className="form container mx-auto flex flex-col  mt-40 items-center bg-white/75 rounded-2xl shadow-xl p-5 shadow-gray-300 border-2 border-white bg-gray-400 gap-10 overflow-hidden">
       <h2 className="text-3xl font-medium text-red-700 mt-3">Quick Message</h2>
@@ -35,4 +35,4 @@ const Form = () => {
   );
 };
 
-export default Form;
+export default Contact;
